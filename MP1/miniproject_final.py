ddbdea1f585abdad43c52a7b834f3771ff950c69
YoungHwa <yhkimmay@gmail.com>
2015-09-29 13:48:31 -0400

    commitinggit add .

---
+++ MP1/miniproject_final.py
@@ -0,0 +1,49 @@
+from pattern.web import *
+
+g = Bing()
+listurl = []
+for result in g.search('Reflection lyrics'):
+	listurl.append(result.url)
+
+url = URL(string=listurl[0])
+
+lyrics = url.download()
+lyrics = plaintext(lyrics)
+
+lyricslist = lyrics.split("\n")
+
+num = 0
+while num<len(lyricslist):
+	text = lyricslist[num]
+	lyricslist[num] = text.encode('ascii','ignore')
+	num = num + 1
+
+
+a = lyricslist.index('Get "Reflection" Ringtone') + 1
+list1 = lyricslist[a:]
+b = list1.index('Songwriters')
+list2 = list1[:b]
+
+
+nm = 0
+while nm<len(list2):
+	string = list2[nm]
+	list2[nm] = string.split()
+	nm = nm + 1
+
+num = 0
+listnew = []
+for i in range(len(list2)):
+	listnew = listnew + list2[i]
+
+
+counts = {}
+for i in listnew:
+	if i in counts:
+		counts[i] += 1
+	else:
+		counts[i] = 1
+
+from collections import Counter
+counter = Counter(counts)
+print counter.most_common()
