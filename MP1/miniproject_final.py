0211a209315f5dca65ad74457d5c3faa3d86fd7a
Anne LoVerso <aelv13@gmail.com>
2015-10-06 10:45:01 -0700

    MP1 grading
    Contains comments and feedback on MP1 program

--- MP1/miniproject_final.py
+++ MP1/miniproject_final.py
@@ -2,6 +2,7 @@ from pattern.web import *
 
 g = Bing()
 listurl = []
+# it would be cool if you took the song title as a user input
 for result in g.search('Reflection lyrics'):
 	listurl.append(result.url)
 
@@ -17,13 +18,19 @@ while num<len(lyricslist):
 	text = lyricslist[num]
 	lyricslist[num] = text.encode('ascii','ignore')
 	num = num + 1
+	# when you use a while loop with a counter like this,
+	# this is exactly the point at which you would use a for loop
+	# it's what they were made for
+	# this works, but a for loop woulld be considered more appropriate
 
 
 a = lyricslist.index('Get "Reflection" Ringtone') + 1
 list1 = lyricslist[a:]
 b = list1.index('Songwriters')
 list2 = list1[:b]
-
+# as you pointed out in your reflection, this is 
+# very specific both to the song you pick and the website you pull from
+# thinking more about ways to make this work always no matter the song or website would be good
 
 nm = 0
 while nm<len(list2):
@@ -31,10 +38,14 @@ while nm<len(list2):
 	list2[nm] = string.split()
 	nm = nm + 1
 
-num = 0
+num = 0 # this variable doesn't get used
 listnew = []
 for i in range(len(list2)):
 	listnew = listnew + list2[i]
+
+# these two loops are well cone and work well to make the list of words you want
+# but in terms of efficiency, you could have combined them and added to listnew
+# inside of the while loop, instead of having two separate loops
 
 
 counts = {}
